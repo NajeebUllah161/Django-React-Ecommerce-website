4f64c0a6bd8b20f46f7ccb4157b9ac68edbf1c3c
unknown <manshoor110@gmail.com>
2022-05-22 23:45:47 +0500

    able to upload product image

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,9 +3,23 @@ import {
     PRODUCTS_LIST_REQUEST,
     PRODUCTS_LIST_SUCCESS,
     PRODUCTS_LIST_FAILURE,
+
     PRODUCT_DETAILS_REQUEST,
     PRODUCT_DETAILS_SUCCESS,
     PRODUCT_DETAILS_FAILURE,
+
+    PRODUCT_DELETE_REQUEST,
+    PRODUCT_DELETE_SUCCESS,
+    PRODUCT_DELETE_FAILURE,
+
+    PRODUCT_CREATE_REQUEST,
+    PRODUCT_CREATE_SUCCESS,
+    PRODUCT_CREATE_FAILURE,
+
+    PRODUCT_UPDATE_REQUEST,
+    PRODUCT_UPDATE_SUCCESS,
+    PRODUCT_UPDATE_FAILURE,
+
 } from '../constants/productConstants'
 
 
@@ -42,7 +56,7 @@ export const listProductDetails = (id) => async (dispatch) => {
         dispatch({
             type: PRODUCT_DETAILS_REQUEST
         })
-        const { data } = await axios.get(`/api/products/${id}`)
+        const { data } = await axios.get(`/api/products/${id}/`)
 
         dispatch({
             type: PRODUCT_DETAILS_SUCCESS,
@@ -58,4 +72,122 @@ export const listProductDetails = (id) => async (dispatch) => {
         })
     }
 
+}
+
+export const deleteProduct = (id) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: PRODUCT_DELETE_REQUEST
+        })
+
+        const { userLogin: { userInfo }
+        } = getState()
+
+        const config = {
+            headers: {
+                'Content-type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const { data } = await axios.delete(
+            `/api/products/delete/${id}/`,
+            config
+        )
+
+        dispatch({
+            type: PRODUCT_DELETE_SUCCESS,
+        })
+
+    }
+    catch (error) {
+        dispatch({
+            type: PRODUCT_DELETE_FAILURE,
+            payload: error.response && error.response.data.detail
+                ? error.message.data.detail
+                : error.message
+        })
+    }
+}
+
+
+export const createProduct = () => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: PRODUCT_CREATE_REQUEST
+        })
+
+        const { userLogin: { userInfo }
+        } = getState()
+
+        const config = {
+            headers: {
+                'Content-type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const { data } = await axios.post(
+            `/api/products/create/`,
+            {},
+            config
+        )
+
+        dispatch({
+            type: PRODUCT_CREATE_SUCCESS,
+            payload: data
+        })
+
+    }
+    catch (error) {
+        dispatch({
+            type: PRODUCT_CREATE_FAILURE,
+            payload: error.response && error.response.data.detail
+                ? error.message.data.detail
+                : error.message
+        })
+    }
+}
+
+export const updateProduct = (product) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: PRODUCT_UPDATE_REQUEST
+        })
+
+        const { userLogin: { userInfo }
+        } = getState()
+
+        const config = {
+            headers: {
+                'Content-type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const { data } = await axios.put(
+            `/api/products/update/${product._id}/`,
+            product,
+            config
+        )
+
+        dispatch({
+            type: PRODUCT_UPDATE_SUCCESS,
+            payload: data
+        })
+
+        dispatch({
+            type: PRODUCT_DETAILS_SUCCESS,
+            payload: data
+        })
+
+    }
+    catch (error) {
+        dispatch({
+            type: PRODUCT_UPDATE_FAILURE,
+            payload: error.response && error.response.data.detail
+                ? error.message.data.detail
+                : error.message
+        })
+    }
 }
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -2,9 +2,24 @@ import {
     PRODUCTS_LIST_REQUEST,
     PRODUCTS_LIST_SUCCESS,
     PRODUCTS_LIST_FAILURE,
+
     PRODUCT_DETAILS_REQUEST,
     PRODUCT_DETAILS_SUCCESS,
     PRODUCT_DETAILS_FAILURE,
+
+    PRODUCT_DELETE_REQUEST,
+    PRODUCT_DELETE_SUCCESS,
+    PRODUCT_DELETE_FAILURE,
+
+    PRODUCT_CREATE_REQUEST,
+    PRODUCT_CREATE_SUCCESS,
+    PRODUCT_CREATE_FAILURE,
+    PRODUCT_CREATE_RESET,
+
+    PRODUCT_UPDATE_REQUEST,
+    PRODUCT_UPDATE_SUCCESS,
+    PRODUCT_UPDATE_FAILURE,
+    PRODUCT_UPDATE_RESET
 
 } from '../constants/productConstants'
 
@@ -15,7 +30,7 @@ export const productListReducer = (state = { products: [] }, action) => {
 
         case PRODUCTS_LIST_SUCCESS:
             return { loading: false, products: action.payload }
-        
+
         case PRODUCTS_LIST_FAILURE:
             return { loading: false, error: action.payload }
 
@@ -42,4 +57,61 @@ export const productDetailsReducer = (state = { product: { reviews: [] } }, acti
             return state
     }
 
+}
+
+export const productDeleteReducer = (state = {}, action) => {
+
+    switch (action.type) {
+        case PRODUCT_DELETE_REQUEST:
+            return { loading: true }
+
+        case PRODUCT_DELETE_SUCCESS:
+            return { loading: false, success: true }
+
+        case PRODUCT_DELETE_FAILURE:
+            return { loading: false, error: action.payload }
+
+        default:
+            return state
+    }
+}
+
+export const productCreateReducer = (state = {}, action) => {
+
+    switch (action.type) {
+        case PRODUCT_CREATE_REQUEST:
+            return { loading: true }
+
+        case PRODUCT_CREATE_SUCCESS:
+            return { loading: false, success: true, product: action.payload }
+
+        case PRODUCT_CREATE_FAILURE:
+            return { loading: false, error: action.payload }
+
+        case PRODUCT_CREATE_RESET:
+            return {}
+
+        default:
+            return state
+    }
+}
+
+export const productUpdateReducer = (state = { product: {} }, action) => {
+
+    switch (action.type) {
+        case PRODUCT_UPDATE_REQUEST:
+            return { loading: true }
+
+        case PRODUCT_UPDATE_SUCCESS:
+            return { loading: false, success: true, product: action.payload }
+
+        case PRODUCT_UPDATE_FAILURE:
+            return { loading: false, error: action.payload }
+
+        case PRODUCT_UPDATE_RESET:
+            return { product: {} }
+
+        default:
+            return state
+    }
 }
